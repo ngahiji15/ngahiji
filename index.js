338a526ff534c99723b95bce834cb46fd3863ad7
ngahiji15 <ashddq.merchant@gmail.com>
2023-07-24 11:29:18 +0700

    change host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express       = require('express');
 const path          = require('path');
 const app           = express();
 
-const host = 'localhostloca';
+const host = 'localhost';
 const port = 80;
 
 const appRouting    = require('./routers/app-route');
